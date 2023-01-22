19557c43900d7acfa2104496961ad55d4978907c
ajbuwidnnfjwj <416tommy@naver.com>
2023-01-22 13:03:55 +0900

    1.22 업로드

---
+++ PlumMiniGame/Assets/CameraResolution.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResolution : MonoBehaviour
+{
+    private void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleheight = ((float)Screen.width / Screen.height) / ((float)9 / 16);
+        float scalewidth = 1f / scaleheight;
+
+        if(scaleheight < 1)
+        {
+            rect.height = scaleheight;
+            rect.y = (1f - scaleheight) / 2f;
+        }
+        else
+        {
+            rect.width = scalewidth;
+            rect.x = (1f - scalewidth) / 2f;
+        }
+        camera.rect = rect;
+    }
+}
