6a1a685cc0a2e136d3398d5ef4945d61db298c50
ajbuwidnnfjwj <416tommy@naver.com>
2023-02-07 16:55:09 +0900

    최상준 점프게임 틸트 재수정

--- PlumMiniGame/Assets/Part1Jump/Scripts/Character.cs
+++ PlumMiniGame/Assets/Part1Jump/Scripts/Character.cs
@@ -7,13 +7,15 @@ public class Character : MonoBehaviour
 
     public static float jump_power;
     public static bool is_shield;
-
+    private bool is_editor;
     [SerializeField]
     private float move_speed;
     [SerializeField]
     private GameObject shield_on_img;
 
     private Rigidbody2D rb;
+
+
     void Start()
     {
         jump_power = 800;
@@ -26,32 +28,37 @@ public class Character : MonoBehaviour
         {
             rb = GetComponent<Rigidbody2D>();
         }
-        
+
+#if UNITY_EDITOR
+        is_editor = true;
+#else
+        is_editor = false;
+#endif
     }
 
     void Update()
     {
         if (JumpGameManager.game_continue)
         {
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            if(is_editor)
             {
-                gameObject.transform.position += new Vector3(move_speed, 0, 0);
+                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+                {
+                    gameObject.transform.position += new Vector3(move_speed, 0, 0);
+                }
+                else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+                {
+                    gameObject.transform.position -= new Vector3(move_speed, 0, 0);
+                }
             }
-            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            else
             {
-                gameObject.transform.position -= new Vector3(move_speed, 0, 0);
-            }
-
-            Vector3 dir = Vector3.zero;
-            dir.x = Input.acceleration.x;
-            if (dir.sqrMagnitude > 1) dir.Normalize();
-            dir *= Time.deltaTime;
-            dir.y = rb.velocity.y;
-            rb.velocity = new Vector2(dir.x * 700, dir.y); //x-axis movement by tilt
-
-            if (JumpGameManager.game_continue)
-            {
-
+                Vector3 dir = Vector3.zero;
+                dir.x = Input.acceleration.x;
+                if (dir.sqrMagnitude > 1) dir.Normalize();
+                dir *= Time.deltaTime;
+                dir.y = rb.velocity.y;
+                rb.velocity = new Vector2(dir.x * move_speed * 700, dir.y); //x-axis movement by tilt
             }
 
             if (is_shield)
