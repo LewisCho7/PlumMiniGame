e00c1a14edbbda4d1bf59aa8876685f679087643
ajbuwidnnfjwj <416tommy@naver.com>
2023-02-21 14:07:57 +0900

    최상준 가구상점

--- PlumMiniGame/Assets/NonMinigame/Store/Scrips/Newsstand.cs
+++ PlumMiniGame/Assets/NonMinigame/Store/Scrips/Newsstand.cs
@@ -5,33 +5,63 @@ using UnityEngine;
 public class Newsstand : MonoBehaviour
 {
     [SerializeField]
-    private GameObject first_floor;
+    private GameObject[] stand_floor;
     [SerializeField]
-    private GameObject second_floor;
+    private GameObject theme_selector;
+    private int current_floor;
+
+    private Sprite[] sprites;
     // Start is called before the first frame update
     void Start()
     {
-        first_floor.GetComponent<SpriteRenderer>().sortingOrder = 4;
-        second_floor.GetComponent<SpriteRenderer>().sortingOrder = 3;
+        stand_floor[0].GetComponent<SpriteRenderer>().sortingOrder = 4;
+        stand_floor[1].GetComponent<SpriteRenderer>().sortingOrder = 3;
+        stand_floor[1].transform.GetChild(0).gameObject.SetActive(false);
 
-        second_floor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+        stand_floor[1].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+
+        theme_selector.SetActive(false);
+
+        current_floor = 0;
     }
 
     public void FirstFloorClicked()
     {
-        first_floor.GetComponent<SpriteRenderer>().sortingOrder = 4;
-        second_floor.GetComponent<SpriteRenderer>().sortingOrder = 3;
+        stand_floor[0].GetComponent<SpriteRenderer>().sortingOrder = 4;
+        stand_floor[1].GetComponent<SpriteRenderer>().sortingOrder = 3;
+        stand_floor[1].transform.GetChild(0).gameObject.SetActive(false);
 
-        first_floor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
-        second_floor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+        stand_floor[0].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+        stand_floor[1].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+
+        current_floor = 0;
     }
 
     public void SecondFloorClicked()
     {
-        first_floor.GetComponent<SpriteRenderer>().sortingOrder = 3;
-        second_floor.GetComponent<SpriteRenderer>().sortingOrder = 4;
+        stand_floor[0].GetComponent<SpriteRenderer>().sortingOrder = 3;
+        stand_floor[0].transform.GetChild(0).gameObject.SetActive(false);
+        stand_floor[1].GetComponent<SpriteRenderer>().sortingOrder = 4;
+        
 
-        first_floor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
-        second_floor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+        stand_floor[0].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+        stand_floor[1].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+
+        current_floor = 1;
+    }
+
+    public void FurnitureSelected(int id)
+    {
+        stand_floor[(int)id / 12].transform.GetChild(0).
+            transform.GetChild(0).gameObject.SetActive(false);
+        string path = "Assets/NonMinigame/Store/Sprite/FurnitureStore" + id.ToString();
+        sprites = Resources.LoadAll<Sprite>(path);
+    }
+
+    public void BackButton()
+    {
+        stand_floor[current_floor].transform.GetChild(0).
+            transform.GetChild(0).gameObject.SetActive(true);
+        sprites = null;
     }
 }
