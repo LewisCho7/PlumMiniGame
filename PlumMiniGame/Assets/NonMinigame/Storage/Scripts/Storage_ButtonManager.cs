1412cf5175a12b518b2d548411b136d7e462b448
조강현 <111622974+LewisCho7@users.noreply.github.com>
2023-02-25 15:49:20 +0900

    조강현 창고 시스템 적용 완료
    조강현 창고 시스템 적용 완료

--- PlumMiniGame/Assets/NonMinigame/Storage/Scripts/Storage_ButtonManager.cs
+++ PlumMiniGame/Assets/NonMinigame/Storage/Scripts/Storage_ButtonManager.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Storage_ButtonManager : MonoBehaviour
 {
+    public static Storage_ButtonManager instance;
     public List<GameObject> storage;
     public GameObject list;
     public GameObject selectButton;
-    public List<List<GameObject>> furnitures;
-    private string string_id;
+    public GameObject background;
+    public string selectedID;
+
+
     private int int_id;
     private bool isList;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Start()
     {
@@ -20,8 +28,9 @@ public class Storage_ButtonManager : MonoBehaviour
     {
         //list 종류에 따라서 바꾸기
         int_id = StorageIndex;
-        string_id = (StorageIndex < 10) ? ("0" + StorageIndex.ToString()) : StorageIndex.ToString();
-        storage[int_id].SetActive(true);
+        //string_id = (StorageIndex < 10) ? ("0" + StorageIndex.ToString()) : StorageIndex.ToString();
+        background.SetActive(true);
+        storage[int_id - 1].SetActive(true);
         list.SetActive(false);
         selectButton.SetActive(false);
         isList = false;
@@ -31,7 +40,8 @@ public class Storage_ButtonManager : MonoBehaviour
     {
         if (!isList)
         {
-            //storage[storageIndex].SetActive(false);
+            storage[int_id - 1].SetActive(false);
+            background.SetActive(false);
             list.SetActive(true);
             selectButton.SetActive(false);
             isList = true;
@@ -40,11 +50,21 @@ public class Storage_ButtonManager : MonoBehaviour
         {
             GameSceneManager.instance.myRoomSceneLoad();
         }
-        
-    }
 
+    }
     public void onClickSelect()
     {
-
+        string id1 = selectedID.Substring(0, 2);
+        foreach(string id in DataManager.instance.saveData.myRoomFurnitures)
+        {
+            if(id.Substring(0, 2) == id1)
+            {
+                DataManager.instance.saveData.myRoomFurnitures.Remove(id);
+                break;
+            }
+        }
+        DataManager.instance.saveData.myRoomFurnitures.Add(selectedID);
+        DataManager.instance.SaveGame();
+        GameSceneManager.instance.myRoomSceneLoad();
     }
 }
