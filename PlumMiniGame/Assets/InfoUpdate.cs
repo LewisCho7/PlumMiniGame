0d7ca6347d85414b8fca4c5fef4cd523ba95a15f
ajbuwidnnfjwj <416tommy@naver.com>
2023-07-20 19:43:31 +0900

    참참참 스코어보드 업데이트

--- PlumMiniGame/Assets/InfoUpdate.cs
+++ PlumMiniGame/Assets/InfoUpdate.cs
@@ -22,12 +22,28 @@ public class InfoUpdate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!JumpGameManager.game_continue)
-        {
-            
+        if (!ChamGameManager.game_on_process)
+        {           
             score.GetComponent<TextMeshProUGUI>().text = TextUI.score.ToString();
-            coinBonus.GetComponent<TextMeshProUGUI>().text = DataManager.instance.saveData.currentBonus.ToString();
-            coin.GetComponent<TextMeshProUGUI>().text = JumpGameManager.CoinCalculate().ToString();
+            coinBonus.GetComponent<TextMeshProUGUI>().text = '+' + DataManager.instance.saveData.currentBonus.ToString() + "%";
+            coin.GetComponent<TextMeshProUGUI>().text = ChamGameManager.CoinCalculate().ToString();
+
+            if (ChamGameManager.hard_mode)
+            {
+                if (TextUI.score > DataManager.instance.saveData.hardHighScore[3])
+                    DataManager.instance.saveData.hardHighScore[3] = TextUI.score;
+
+                bestScore.GetComponent<TextMeshProUGUI>().text
+                    = DataManager.instance.saveData.hardHighScore[3].ToString();
+            }
+            else
+            {
+                if (TextUI.score > DataManager.instance.saveData.normalHighScore[3])
+                    DataManager.instance.saveData.normalHighScore[3] = TextUI.score;
+
+                bestScore.GetComponent<TextMeshProUGUI>().text
+                    = DataManager.instance.saveData.normalHighScore[3].ToString();
+            }
         }
     }
 }
